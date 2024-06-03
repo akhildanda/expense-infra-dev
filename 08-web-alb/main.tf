75e66c8e0835cb9c2008333456e210b203469c5c
akhildanda <akhildanda7@gmail.com>
2024-06-03 14:25:40 +0530

    ansible

--- 08-web-alb/main.tf
+++ 08-web-alb/main.tf
@@ -2,20 +2,20 @@ resource "aws_lb" "web-alb" {
   name               = "${var.project_name}-${var.environment}-web-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_ssm_parameter.app-alb_sg_id.value]
-  subnets            = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  security_groups    = [data.aws_ssm_parameter.web-alb_sg_id.value]
+  subnets            = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
 
   enable_deletion_protection = false
  tags = merge(
     var.common_tags,
     {
-        Name = "${var.project_name}-${var.environment}-app-alb"
+        Name = "${var.project_name}-${var.environment}-web-alb"
     }
   )
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.app-alb.arn
+  load_balancer_arn = aws_lb.web-alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -24,7 +24,7 @@ resource "aws_lb_listener" "http" {
 
     fixed_response {
       content_type = "text/html"
-      message_body = "<h1>This is fixed response from APP ALB</h1>"
+      message_body = "<h1>This is fixed response from WEB ALB</h1>"
       status_code  = "200"
     }
   }
@@ -38,12 +38,12 @@ module "records" {
 
   records = [
     {
-      name    = "*.app-${var.environment}"
+      name    = "web-${var.environment}"
       type    = "A"
       allow_overwrite = true
       alias  = {
-        name = aws_lb.app-alb.dns_name
-        zone_id = aws_lb.app-alb.zone_id
+        name = aws_lb.web-alb.dns_name
+        zone_id = aws_lb.web-alb.zone_id
       }
     }
   ]
